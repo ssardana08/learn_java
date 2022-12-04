b246ff330c1af494dbb2d5269ea491b5a2721121
Sumit Sardana <sardana.sumit08@gmail.com>
2022-12-05 02:51:11 +0530

    - sum of numbers added

--- FirstJavaProgram/src/Main.java
+++ FirstJavaProgram/src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
+
+    public static void sum(){
+
+        System.out.println("Sum of numbers!");
+
+        int num1 = 3;
+        int num2 = 5;
+        int num3 = 9;
+        int sum = num1 + num2 + num3;
+        System.out.println(sum);
+    }
+
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        sum();
     }
 }
