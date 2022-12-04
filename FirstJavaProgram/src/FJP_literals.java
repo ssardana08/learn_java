7e8b41dedc5f42655532883b666758d3c4284b24
Sumit Sardana <sardana.sumit08@gmail.com>
2022-12-05 03:02:08 +0530

    - literals added

---
+++ FirstJavaProgram/src/FJP_literals.java
@@ -0,0 +1,12 @@
+public class FJP_literals {
+    public static void main(String[] args) {
+        byte age = 26;
+        char ch = 'A';
+        float f1 = 4.6f;
+        double d1 = 5.67d;
+        boolean a = true;
+        String name = "Sumit";
+
+        System.out.println(name);
+    }
+}
