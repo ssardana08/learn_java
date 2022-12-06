3b1d11b93a87f8cbafc27561ace4b2f34ede0fea
Sumit Sardana <sardana.sumit08@gmail.com>
2022-12-07 02:01:30 +0530

    - switch conditional

---
+++ FirstJavaProgram/src/FJP_switch_case.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class FJP_switch_case {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Please enter URL: ");
+        String url = sc.next();
+
+        String protocol = url.split(":")[0];
+        switch (protocol){
+            case "http":
+                System.out.println("Not Secured!");
+                break;
+            case "https":
+                System.out.println("Secured");
+                break;
+            default:
+                System.out.println("Invalid Protocol");
+        }
+    }
+}
