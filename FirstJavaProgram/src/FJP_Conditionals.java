0b94a373e1a3f5f784ea989b9a24272f2f287569
Sumit Sardana <sardana.sumit08@gmail.com>
2022-12-07 01:46:52 +0530

    - if else conditions

---
+++ FirstJavaProgram/src/FJP_Conditionals.java
@@ -0,0 +1,14 @@
+public class FJP_Conditionals {
+    public static void main(String[] args) {
+        int[] ages = {12, 20, 61, 5};
+        for (int age : ages) {
+            if (age > 50) {
+                System.out.println("Too old");
+            } else if (age > 18) {
+                System.out.println("You are eligible");
+            } else {
+                System.out.println("Not eligible");
+            }
+        }
+    }
+}
