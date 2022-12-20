bd97d13ee94d754ee52a6c5fb7970c15a0bb9be4
Sumit Sardana <sardana.sumit08@gmail.com>
2022-12-21 02:05:52 +0530

    - default method + polymorphism

---
+++ FirstJavaProgram/src/com/fjp/FJP_default_method_interface.java
@@ -0,0 +1,57 @@
+package com.fjp;
+
+interface Camera{
+    void takePic();
+    void recordVideo();
+    private void greet(){
+        System.out.println("Good Morning");
+    }
+    default void record4kVideo(){
+        System.out.println("Recording 4K Video");
+    }
+}
+
+interface GPS{
+    void getNetwork();
+}
+
+class CellPhone {
+    void acceptCall(){
+        System.out.println("Accepting Call");
+    }
+
+    void dial(int phoneNumber){
+        System.out.println("Dialling: " + phoneNumber + " ...");
+    }
+
+    void rejectCall(){
+        System.out.println("Rejecting Call");
+    }
+}
+
+class SmartPhone extends CellPhone implements Camera, GPS{
+    @Override
+    public void getNetwork() {
+        System.out.println("Getting network");
+    }
+
+    @Override
+    public void takePic() {
+        System.out.println("Say Cheese!");
+    }
+
+    @Override
+    public void recordVideo() {
+        System.out.println("Lights Camera Action!");
+    }
+}
+
+public class FJP_default_method_interface {
+    public static void main(String[] args) {
+        SmartPhone sp = new SmartPhone();
+        sp.recordVideo();
+
+        GPS g = new SmartPhone();
+        g.getNetwork();
+    }
+}
